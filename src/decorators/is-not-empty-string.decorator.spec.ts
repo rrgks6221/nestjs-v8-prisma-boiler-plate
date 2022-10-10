c2e9236aeb5e9de36518ae854b7cb94bc1e451b6
LeeSeokHo <rrgks@naver.com>
2022-10-10 18:42:14 +0900

    Test: IsNotEmptyString decorator 테스트코드

---
+++ src/decorators/is-not-empty-string.decorator.spec.ts
@@ -0,0 +1,72 @@
+import { IsNotEmptyString } from '@src/decorators/is-not-empty-string.decorator';
+import { faker } from '@faker-js/faker';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+
+describe('IsNotEmptyString decorator', () => {
+  let randomString;
+
+  class TestClass {
+    @IsNotEmptyString('test')
+    test;
+  }
+
+  it('비어있지 않은 string 이 들어왔을 경우', async () => {
+    randomString = faker.datatype.string();
+
+    const validation = plainToInstance(TestClass, { test: randomString });
+
+    const errors = await validate(validation);
+
+    expect(errors).toHaveLength(0);
+    expect(randomString).not.toHaveLength(0);
+  });
+
+  it('공백을 포함한 비어있지 않은 string 이 들어왔을 경우', async () => {
+    randomString = faker.datatype.string();
+    randomString = ' ' + randomString + ' ';
+
+    const validation = plainToInstance(TestClass, { test: randomString });
+
+    const errors = await validate(validation);
+
+    expect(errors).toHaveLength(0);
+    expect(randomString).not.toHaveLength(0);
+  });
+
+  it('빈 string 이 들어왔을 경우', async () => {
+    randomString = '';
+
+    const validation = plainToInstance(TestClass, { test: randomString });
+
+    const errors = await validate(validation);
+
+    expect(errors).toHaveLength(1);
+    expect(randomString).toHaveLength(0);
+  });
+
+  it('공백만 존재하는 string 이 들어왔을 경우', async () => {
+    randomString = '     ';
+
+    const trimRandomString = randomString.trim();
+
+    const validation = plainToInstance(TestClass, { test: randomString });
+
+    const errors = await validate(validation);
+
+    expect(errors).toHaveLength(1);
+    expect(randomString).not.toHaveLength(0);
+    expect(trimRandomString).toHaveLength(0);
+  });
+
+  it('string 타입이 아닌 다른 타입이 들어왔을 경우 이 들어왔을 경우', async () => {
+    const NotStringType = faker.datatype.number();
+
+    const validation = plainToInstance(TestClass, { test: NotStringType });
+
+    const errors = await validate(validation);
+
+    expect(errors).toHaveLength(0);
+    expect(typeof NotStringType).not.toBe('string');
+  });
+});
